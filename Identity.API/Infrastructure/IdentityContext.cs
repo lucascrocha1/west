eddc1e321df078f739d8c979f804549f115edac6
Lucas Rocha <lscrweb@gmail.com>
2020-02-11 13:26:37 -0300

    Adds identity project

---
+++ Identity.API/Infrastructure/IdentityContext.cs
@@ -0,0 +1,19 @@
+﻿namespace Identity.API.Infrastructure
+{
+    using Identity.API.Model;
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+
+    public class IdentityContext : IdentityDbContext<ApplicationUser>
+    {
+        public IdentityContext(DbContextOptions<IdentityContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ Identity.API/Model/ApplicationUser.cs
@@ -0,0 +1,19 @@
+﻿namespace Identity.API.Model
+{
+    using Microsoft.AspNetCore.Identity;
+
+    public class ApplicationUser : IdentityUser
+    {
+        public string Street { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        public string Country { get; set; }
+
+        public string ZipCode { get; set; }
+
+        public string Name { get; set; }
+    }
+}
