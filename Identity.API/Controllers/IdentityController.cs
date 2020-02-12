66d47b567eedb27ab0dd1128b8db6e8e6ea76603
Lucas Rocha <lscrweb@gmail.com>
2020-02-11 23:38:08 -0300

    Adds identity controller

---
+++ Identity.API/Controllers/IdentityController.cs
@@ -0,0 +1,13 @@
+﻿namespace Identity.API.Controllers
+{
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class IdentityController : ControllerBase
+    {
+
+    }
+}
